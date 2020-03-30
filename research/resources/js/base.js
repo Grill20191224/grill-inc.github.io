97b619fb8517978e0770b104e56048dcbf5465c6
HAL <haruya@fusion.to>
2020-03-30 19:03:38 +0900

    [修正] グッテレ／リサーチ：問い合わせをモーダルから別タブへ変更

--- research/resources/js/base.js
+++ research/resources/js/base.js
@@ -19,25 +19,25 @@ jQuery(function($){
 	if (w <= x) {
 		$('body').addClass('mode__sp');
         $('body').removeClass('mode__pc');
-        
-        
-        
+
+
+
 	} else {
-        
+
         $('body').removeClass('mode__sp');
         $('body').addClass('mode__pc');
-        
-       
-        
+
+
+
 	}
-    
+
     //*-- ページ内スクロール --------------------------------*
     $(document).on('click','.scrolltop', function() {
         var targetY = $($(this).attr('href')).offset().top;
 		$('html,body').animate({ scrollTop: targetY }, 1000);
 		return false;
     });
-    
+
     //*-- privacy modal --------------------------------*
     var current_scrollY;
     $('.privacy__modal').css('display', 'none');
@@ -54,12 +54,13 @@ jQuery(function($){
 	});
 
     $('.privacy__modal .clsBtn').on('click',function(){
-        $('.privacy__modal').fadeOut(300); 
-        $('.privacy__modal').removeClass('open'); 
+        $('.privacy__modal').fadeOut(300);
+        $('.privacy__modal').removeClass('open');
         $( 'html, body' ).removeAttr('style');
         $( 'html, body' ).prop( { scrollTop: current_scrollY } );
     });
-    
+
+	/*
     $('.contact__modal').css('display', 'none');
     $('.contact__modal iframe').after('<span class="clsBtn"></span>');
     $('.contactBtn').on('click',function(){
@@ -74,17 +75,10 @@ jQuery(function($){
 	});
 
     $('.contact__modal .clsBtn').on('click',function(){
-        $('.contact__modal').fadeOut(300); 
-        $('.contact__modal').removeClass('open'); 
+        $('.contact__modal').fadeOut(300);
+        $('.contact__modal').removeClass('open');
         $( 'html, body' ).removeAttr('style');
         $( 'html, body' ).prop( { scrollTop: current_scrollY } );
     });
+	*/
 });
-
-
-
-
- 
- 
-
-
